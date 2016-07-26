runcom/ocitools

Use oci-seccomp-gen library for adding seccomp configurations
Signed-off-by: Grantseltzer <grantseltzer@gmail.com>

contrib/oci-seccomp-gen/consts.go
@@ -1,0 +1,7 @@
+package ociseccompgen
+
+const (
+	overwrite = "overwrite"
+	appnd     = "append"
+	nothing   = "nothing"
+)

contrib/oci-seccomp-gen/parse_action.go
@@ -1,0 +1,106 @@
+package ociseccompgen
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// ParseSyscallFlag takes the name of the action, the arguments (syscalls) that were
+// passed with it at the command line and a pointer to the config struct. It parses
+// the action and syscalls and updates the config accordingly
+func ParseSyscallFlag(action string, arguments string, config *types.Seccomp) error {
+
+	if arguments == "" {
+		return nil
+	}
+
+	syscallArgs := strings.Split(arguments, ",")
+
+	correctedAction, err := parseAction(action)
+	if err != nil {
+		return err
+	}
+
+	if correctedAction == config.DefaultAction {
+		logrus.Info("Action is already set as default")
+		return nil
+	}
+
+	for _, syscallArg := range syscallArgs {
+		delimArgs := strings.Split(syscallArg, ":")
+		argSlice, err := parseArguments(delimArgs)
+		if err != nil {
+			return err
+		}
+
+		newSyscall := newSyscallStruct(delimArgs[0], correctedAction, *argSlice)
+		descison, err := decideCourseOfAction(&newSyscall, config.Syscalls)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		delimDescison := strings.Split(descison, ":")
+
+		if delimDescison[0] == nothing {
+			logrus.Info("No action taken: ", newSyscall)
+		}
+
+		if delimDescison[0] == appnd {
+			config.Syscalls = append(config.Syscalls, newSyscall)
+		}
+
+		if delimDescison[0] == overwrite {
+			indexForOverwrite, err := strconv.ParseInt(delimDescison[1], 10, 32)
+			if err != nil {
+				return err
+			}
+			config.Syscalls[indexForOverwrite] = newSyscall
+		}
+	}
+	return nil
+}
+
+var actions = map[string]types.Action{
+	"allow": types.ActAllow,
+	"errno": types.ActErrno,
+	"kill":  types.ActKill,
+	"trace": types.ActTrace,
+	"trap":  types.ActTrap,
+}
+
+// Take passed action, return the SCMP_ACT_<ACTION> version of it
+func parseAction(action string) (types.Action, error) {
+	a, ok := actions[action]
+	if !ok {
+		return "", fmt.Errorf("Unrecognized action: %s", action)
+	}
+	return a, nil
+}
+
+//ParseDefaultAction simply sets the default action of the seccomp configuration
+func ParseDefaultAction(action string, config *types.Seccomp) error {
+
+	if action == "" {
+		return nil
+	}
+
+	defaultAction, err := parseAction(action)
+	if err != nil {
+		return err
+	}
+	config.DefaultAction = defaultAction
+	return nil
+}
+
+func newSyscallStruct(name string, action types.Action, args []types.Arg) types.Syscall {
+	syscallStruct := types.Syscall{
+		Name:   name,
+		Action: action,
+		Args:   args,
+	}
+	return syscallStruct
+}

contrib/oci-seccomp-gen/parse_architecture.go
@@ -1,0 +1,60 @@
+package ociseccompgen
+
+import (
+	"fmt"
+	"strings"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// ParseArchitectureFlag takes the raw string passed with the --arch flag, parses it
+// and updates the Seccomp config accordingly
+func ParseArchitectureFlag(architectures string, config *types.Seccomp) error {
+
+	architectureArgs := strings.Split(architectures, ",")
+
+	var arches []types.Arch
+	for _, arg := range architectureArgs {
+		correctedArch, err := parseArch(arg)
+		if err != nil {
+			return err
+		}
+		shouldAppend := true
+		for _, alreadySpecified := range config.Architectures {
+			if correctedArch == alreadySpecified {
+				shouldAppend = false
+			}
+		}
+		if shouldAppend {
+			arches = append(arches, correctedArch)
+			config.Architectures = arches
+		}
+	}
+	return nil
+}
+
+func parseArch(arch string) (types.Arch, error) {
+	arches := map[string]types.Arch{
+		"x86":         types.ArchX86,
+		"amd64":       types.ArchX86_64,
+		"x32":         types.ArchX32,
+		"arm":         types.ArchARM,
+		"arm64":       types.ArchAARCH64,
+		"mips":        types.ArchMIPS,
+		"mips64":      types.ArchMIPS64,
+		"mips64n32":   types.ArchMIPS64N32,
+		"mipsel":      types.ArchMIPSEL,
+		"mipsel64":    types.ArchMIPSEL64,
+		"mipsel64n32": types.ArchMIPSEL64N32,
+		"ppc":         types.ArchPPC,
+		"ppc64":       types.ArchPPC64,
+		"ppc64le":     types.ArchPPC64LE,
+		"s390":        types.ArchS390,
+		"s390x":       types.ArchS390X,
+	}
+	a, ok := arches[arch]
+	if !ok {
+		return "", fmt.Errorf("Unrecognized architecutre: %s", arch)
+	}
+	return a, nil
+}

contrib/oci-seccomp-gen/parse_arguments.go
@@ -1,0 +1,71 @@
+package ociseccompgen
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// parseArguments takes a list of arguments (delimArgs)  and a pointer to a
+// corresponding syscall struct. It parses and fills out the argument information
+func parseArguments(delimArgs []string) (*[]types.Arg, error) {
+	nilArgSlice := new([]types.Arg)
+
+	if len(delimArgs) == 1 {
+		return nilArgSlice, nil
+	}
+
+	if len(delimArgs) == 5 {
+		syscallIndex, err := strconv.ParseUint(delimArgs[1], 10, 0)
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		syscallValue, err := strconv.ParseUint(delimArgs[2], 10, 64)
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		syscallValueTwo, err := strconv.ParseUint(delimArgs[3], 10, 64)
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		syscallOp, err := parseOperator(delimArgs[4])
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		argStruct := types.Arg{
+			Index:    uint(syscallIndex),
+			Value:    syscallValue,
+			ValueTwo: syscallValueTwo,
+			Op:       syscallOp,
+		}
+
+		argSlice := new([]types.Arg)
+		*argSlice = append(*argSlice, argStruct)
+		return argSlice, nil
+	}
+
+	return nilArgSlice, errors.New("Incorrect number of arguments passed with syscall")
+}
+
+func parseOperator(operator string) (types.Operator, error) {
+	operators := map[string]types.Operator{
+		"NE": types.OpNotEqual,
+		"LT": types.OpLessThan,
+		"LE": types.OpLessEqual,
+		"EQ": types.OpEqualTo,
+		"GE": types.OpGreaterEqual,
+		"GT": types.OpGreaterThan,
+		"ME": types.OpMaskedEqual,
+	}
+	o, ok := operators[operator]
+	if !ok {
+		return "", fmt.Errorf("Unrecognized operator: %s", operator)
+	}
+	return o, nil
+}

contrib/oci-seccomp-gen/parse_remove.go
@@ -1,0 +1,33 @@
+package ociseccompgen
+
+import (
+	"fmt"
+	"strings"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// RemoveAction takes the argument string that was passed with the --remove flag,
+// parses it, and updates the Seccomp config accordingly
+func RemoveAction(arguments string, config *types.Seccomp) error {
+
+	if config == nil {
+		return fmt.Errorf("Cannot remove action from nil Seccomp pointer")
+	}
+
+	var syscallsToRemove []string
+	if strings.Contains(arguments, ",") {
+		syscallsToRemove = strings.Split(arguments, ",")
+	} else {
+		syscallsToRemove = append(syscallsToRemove, arguments)
+	}
+
+	for _, syscall := range syscallsToRemove {
+		for counter, syscallStruct := range config.Syscalls {
+			if syscallStruct.Name == syscall {
+				config.Syscalls = append(config.Syscalls[:counter], config.Syscalls[counter+1:]...)
+			}
+		}
+	}
+	return nil
+}

contrib/oci-seccomp-gen/syscall_compare.go
@@ -1,0 +1,145 @@
+package ociseccompgen
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+/*******************************************************
+This file is a bunch of helper functions for determining
+what action should be taken for new syscalls.
+See github.com/grantseltzer/manhattan/Resources/Logic.png
+for a flowchart and explanation
+********************************************************/
+
+func decideCourseOfAction(newSyscall *types.Syscall, syscalls []types.Syscall) (string, error) {
+	ruleForSyscallAlreadyExists := false
+
+	var sliceOfDeterminedActions []string
+	for i, syscall := range syscalls {
+		if syscall.Name == newSyscall.Name {
+			ruleForSyscallAlreadyExists = true
+
+			if identical(newSyscall, &syscall) {
+				sliceOfDeterminedActions = append(sliceOfDeterminedActions, nothing)
+			}
+
+			if sameAction(newSyscall, &syscall) {
+				if bothHaveArgs(newSyscall, &syscall) {
+					sliceOfDeterminedActions = append(sliceOfDeterminedActions, appnd)
+				}
+				if onlyOneHasArgs(newSyscall, &syscall) {
+					if firstParamOnlyHasArgs(newSyscall, &syscall) {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, "overwrite:"+strconv.Itoa(i))
+					} else {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, nothing)
+					}
+				}
+			}
+
+			if !sameAction(newSyscall, &syscall) {
+				if bothHaveArgs(newSyscall, &syscall) {
+					if sameArgs(newSyscall, &syscall) {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, "overwrite:"+strconv.Itoa(i))
+					}
+					if !sameArgs(newSyscall, &syscall) {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, appnd)
+					}
+				}
+				if onlyOneHasArgs(newSyscall, &syscall) {
+					sliceOfDeterminedActions = append(sliceOfDeterminedActions, appnd)
+				}
+				if neitherHasArgs(newSyscall, &syscall) {
+					sliceOfDeterminedActions = append(sliceOfDeterminedActions, "overwrite:"+strconv.Itoa(i))
+				}
+			}
+		}
+	}
+
+	if !ruleForSyscallAlreadyExists {
+		sliceOfDeterminedActions = append(sliceOfDeterminedActions, appnd)
+	}
+
+	// Nothing has highest priority
+	for _, determinedAction := range sliceOfDeterminedActions {
+		if determinedAction == nothing {
+			return determinedAction, nil
+		}
+	}
+
+	// Overwrite has second highest priority
+	for _, determinedAction := range sliceOfDeterminedActions {
+		if strings.Contains(determinedAction, overwrite) {
+			return determinedAction, nil
+		}
+	}
+
+	// Append has the lowest priority
+	for _, determinedAction := range sliceOfDeterminedActions {
+		if determinedAction == appnd {
+			return determinedAction, nil
+		}
+	}
+
+	return "error", fmt.Errorf("Trouble determining action: %s", sliceOfDeterminedActions)
+}
+
+func hasArguments(config *types.Syscall) bool {
+	nilSyscall := new(types.Syscall)
+	return !sameArgs(nilSyscall, config)
+}
+
+func identical(config1, config2 *types.Syscall) bool {
+	return reflect.DeepEqual(config1, config2)
+}
+
+func identicalExceptAction(config1, config2 *types.Syscall) bool {
+	samename := sameName(config1, config2)
+	sameAction := sameAction(config1, config2)
+	sameArgs := sameArgs(config1, config2)
+
+	return samename && !sameAction && sameArgs
+}
+
+func identicalExceptArgs(config1, config2 *types.Syscall) bool {
+	samename := sameName(config1, config2)
+	sameAction := sameAction(config1, config2)
+	sameArgs := sameArgs(config1, config2)
+
+	return samename && sameAction && !sameArgs
+}
+
+func sameName(config1, config2 *types.Syscall) bool {
+	return config1.Name == config2.Name
+}
+
+func sameAction(config1, config2 *types.Syscall) bool {
+	return config1.Action == config2.Action
+}
+
+func sameArgs(config1, config2 *types.Syscall) bool {
+	return reflect.DeepEqual(config1.Args, config2.Args)
+}
+
+func bothHaveArgs(config1, config2 *types.Syscall) bool {
+	return hasArguments(config1) && hasArguments(config2)
+}
+
+func onlyOneHasArgs(config1, config2 *types.Syscall) bool {
+	conf1 := hasArguments(config1)
+	conf2 := hasArguments(config2)
+
+	return (conf1 && !conf2) || (!conf1 && conf2)
+}
+
+func neitherHasArgs(config1, config2 *types.Syscall) bool {
+	return !hasArguments(config1) && !hasArguments(config2)
+}
+
+func firstParamOnlyHasArgs(config1, config2 *types.Syscall) bool {
+	return !hasArguments(config1) && hasArguments(config2)
+}

wking/ocitools-v2

generate/seccomp/consts.go
@@ -1,0 +1,7 @@
+package seccomp
+
+const (
+	seccompOverwrite = "overwrite"
+	seccompAppend    = "append"
+	nothing          = "nothing"
+)

generate/seccomp/parse_action.go
@@ -1,0 +1,104 @@
+package seccomp
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// ParseSyscallFlag takes the name of the action, the arguments (syscalls) that were
+// passed with it at the command line and a pointer to the config struct. It parses
+// the action and syscalls and updates the config accordingly
+func ParseSyscallFlag(action string, arguments string, config *types.Seccomp) error {
+	if arguments == "" {
+		return nil
+	}
+
+	syscallArgs := strings.Split(arguments, ",")
+
+	correctedAction, err := parseAction(action)
+	if err != nil {
+		return err
+	}
+
+	if correctedAction == config.DefaultAction {
+		logrus.Info("Action is already set as default")
+		return nil
+	}
+
+	for _, syscallArg := range syscallArgs {
+		delimArgs := strings.Split(syscallArg, ":")
+		argSlice, err := parseArguments(delimArgs)
+		if err != nil {
+			return err
+		}
+
+		newSyscall := newSyscallStruct(delimArgs[0], correctedAction, *argSlice)
+		descison, err := decideCourseOfAction(&newSyscall, config.Syscalls)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		delimDescison := strings.Split(descison, ":")
+
+		if delimDescison[0] == nothing {
+			logrus.Info("No action taken: ", newSyscall)
+		}
+
+		if delimDescison[0] == seccompAppend {
+			config.Syscalls = append(config.Syscalls, newSyscall)
+		}
+
+		if delimDescison[0] == seccompOverwrite {
+			indexForOverwrite, err := strconv.ParseInt(delimDescison[1], 10, 32)
+			if err != nil {
+				return err
+			}
+			config.Syscalls[indexForOverwrite] = newSyscall
+		}
+	}
+	return nil
+}
+
+var actions = map[string]types.Action{
+	"allow": types.ActAllow,
+	"errno": types.ActErrno,
+	"kill":  types.ActKill,
+	"trace": types.ActTrace,
+	"trap":  types.ActTrap,
+}
+
+// Take passed action, return the SCMP_ACT_<ACTION> version of it
+func parseAction(action string) (types.Action, error) {
+	a, ok := actions[action]
+	if !ok {
+		return "", fmt.Errorf("Unrecognized action: %s", action)
+	}
+	return a, nil
+}
+
+//ParseDefaultAction simply sets the default action of the seccomp configuration
+func ParseDefaultAction(action string, config *types.Seccomp) error {
+	if action == "" {
+		return nil
+	}
+
+	defaultAction, err := parseAction(action)
+	if err != nil {
+		return err
+	}
+	config.DefaultAction = defaultAction
+	return nil
+}
+
+func newSyscallStruct(name string, action types.Action, args []types.Arg) types.Syscall {
+	syscallStruct := types.Syscall{
+		Name:   name,
+		Action: action,
+		Args:   args,
+	}
+	return syscallStruct
+}

generate/seccomp/parse_architecture.go
@@ -1,0 +1,60 @@
+package seccomp
+
+import (
+	"fmt"
+	"strings"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// ParseArchitectureFlag takes the raw string passed with the --arch flag, parses it
+// and updates the Seccomp config accordingly
+func ParseArchitectureFlag(architectures string, config *types.Seccomp) error {
+
+	architectureArgs := strings.Split(architectures, ",")
+
+	var arches []types.Arch
+	for _, arg := range architectureArgs {
+		correctedArch, err := parseArch(arg)
+		if err != nil {
+			return err
+		}
+		shouldAppend := true
+		for _, alreadySpecified := range config.Architectures {
+			if correctedArch == alreadySpecified {
+				shouldAppend = false
+			}
+		}
+		if shouldAppend {
+			arches = append(arches, correctedArch)
+			config.Architectures = arches
+		}
+	}
+	return nil
+}
+
+func parseArch(arch string) (types.Arch, error) {
+	arches := map[string]types.Arch{
+		"x86":         types.ArchX86,
+		"amd64":       types.ArchX86_64,
+		"x32":         types.ArchX32,
+		"arm":         types.ArchARM,
+		"arm64":       types.ArchAARCH64,
+		"mips":        types.ArchMIPS,
+		"mips64":      types.ArchMIPS64,
+		"mips64n32":   types.ArchMIPS64N32,
+		"mipsel":      types.ArchMIPSEL,
+		"mipsel64":    types.ArchMIPSEL64,
+		"mipsel64n32": types.ArchMIPSEL64N32,
+		"ppc":         types.ArchPPC,
+		"ppc64":       types.ArchPPC64,
+		"ppc64le":     types.ArchPPC64LE,
+		"s390":        types.ArchS390,
+		"s390x":       types.ArchS390X,
+	}
+	a, ok := arches[arch]
+	if !ok {
+		return "", fmt.Errorf("Unrecognized architecutre: %s", arch)
+	}
+	return a, nil
+}

generate/seccomp/parse_arguments.go
@@ -1,0 +1,71 @@
+package seccomp
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// parseArguments takes a list of arguments (delimArgs). It parses and fills out
+// the argument information and returns a slice of arg structs
+func parseArguments(delimArgs []string) (*[]types.Arg, error) {
+	nilArgSlice := new([]types.Arg)
+
+	if len(delimArgs) == 1 {
+		return nilArgSlice, nil
+	}
+
+	if len(delimArgs) == 5 {
+		syscallIndex, err := strconv.ParseUint(delimArgs[1], 10, 0)
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		syscallValue, err := strconv.ParseUint(delimArgs[2], 10, 64)
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		syscallValueTwo, err := strconv.ParseUint(delimArgs[3], 10, 64)
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		syscallOp, err := parseOperator(delimArgs[4])
+		if err != nil {
+			return nilArgSlice, err
+		}
+
+		argStruct := types.Arg{
+			Index:    uint(syscallIndex),
+			Value:    syscallValue,
+			ValueTwo: syscallValueTwo,
+			Op:       syscallOp,
+		}
+
+		argSlice := new([]types.Arg)
+		*argSlice = append(*argSlice, argStruct)
+		return argSlice, nil
+	}
+
+	return nilArgSlice, errors.New("Incorrect number of arguments passed with syscall")
+}
+
+func parseOperator(operator string) (types.Operator, error) {
+	operators := map[string]types.Operator{
+		"NE": types.OpNotEqual,
+		"LT": types.OpLessThan,
+		"LE": types.OpLessEqual,
+		"EQ": types.OpEqualTo,
+		"GE": types.OpGreaterEqual,
+		"GT": types.OpGreaterThan,
+		"ME": types.OpMaskedEqual,
+	}
+	o, ok := operators[operator]
+	if !ok {
+		return "", fmt.Errorf("Unrecognized operator: %s", operator)
+	}
+	return o, nil
+}

generate/seccomp/parse_remove.go
@@ -1,0 +1,33 @@
+package seccomp
+
+import (
+	"fmt"
+	"strings"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+// RemoveAction takes the argument string that was passed with the --remove flag,
+// parses it, and updates the Seccomp config accordingly
+func RemoveAction(arguments string, config *types.Seccomp) error {
+
+	if config == nil {
+		return fmt.Errorf("Cannot remove action from nil Seccomp pointer")
+	}
+
+	var syscallsToRemove []string
+	if strings.Contains(arguments, ",") {
+		syscallsToRemove = strings.Split(arguments, ",")
+	} else {
+		syscallsToRemove = append(syscallsToRemove, arguments)
+	}
+
+	for _, syscall := range syscallsToRemove {
+		for counter, syscallStruct := range config.Syscalls {
+			if syscallStruct.Name == syscall {
+				config.Syscalls = append(config.Syscalls[:counter], config.Syscalls[counter+1:]...)
+			}
+		}
+	}
+	return nil
+}

generate/seccomp/syscall_compare.go
@@ -1,0 +1,143 @@
+/*******************************************************************************
+This file contains functions for determining the action to be taken for adding
+				syscalls rules to the seccomp configuration.
+*******************************************************************************/
+
+package seccomp
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	types "github.com/opencontainers/runtime-spec/specs-go"
+)
+
+func decideCourseOfAction(newSyscall *types.Syscall, syscalls []types.Syscall) (string, error) {
+	ruleForSyscallAlreadyExists := false
+
+	var sliceOfDeterminedActions []string
+	for i, syscall := range syscalls {
+		if syscall.Name == newSyscall.Name {
+			ruleForSyscallAlreadyExists = true
+
+			if identical(newSyscall, &syscall) {
+				sliceOfDeterminedActions = append(sliceOfDeterminedActions, nothing)
+			}
+
+			if sameAction(newSyscall, &syscall) {
+				if bothHaveArgs(newSyscall, &syscall) {
+					sliceOfDeterminedActions = append(sliceOfDeterminedActions, seccompAppend)
+				}
+				if onlyOneHasArgs(newSyscall, &syscall) {
+					if firstParamOnlyHasArgs(newSyscall, &syscall) {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, "overwrite:"+strconv.Itoa(i))
+					} else {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, nothing)
+					}
+				}
+			}
+
+			if !sameAction(newSyscall, &syscall) {
+				if bothHaveArgs(newSyscall, &syscall) {
+					if sameArgs(newSyscall, &syscall) {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, "overwrite:"+strconv.Itoa(i))
+					}
+					if !sameArgs(newSyscall, &syscall) {
+						sliceOfDeterminedActions = append(sliceOfDeterminedActions, seccompAppend)
+					}
+				}
+				if onlyOneHasArgs(newSyscall, &syscall) {
+					sliceOfDeterminedActions = append(sliceOfDeterminedActions, seccompAppend)
+				}
+				if neitherHasArgs(newSyscall, &syscall) {
+					sliceOfDeterminedActions = append(sliceOfDeterminedActions, "overwrite:"+strconv.Itoa(i))
+				}
+			}
+		}
+	}
+
+	if !ruleForSyscallAlreadyExists {
+		sliceOfDeterminedActions = append(sliceOfDeterminedActions, seccompAppend)
+	}
+
+	// Nothing has highest priority
+	for _, determinedAction := range sliceOfDeterminedActions {
+		if determinedAction == nothing {
+			return determinedAction, nil
+		}
+	}
+
+	// Overwrite has second highest priority
+	for _, determinedAction := range sliceOfDeterminedActions {
+		if strings.Contains(determinedAction, seccompOverwrite) {
+			return determinedAction, nil
+		}
+	}
+
+	// Append has the lowest priority
+	for _, determinedAction := range sliceOfDeterminedActions {
+		if determinedAction == seccompAppend {
+			return determinedAction, nil
+		}
+	}
+
+	return "error", fmt.Errorf("Trouble determining action: %s", sliceOfDeterminedActions)
+}
+
+func hasArguments(config *types.Syscall) bool {
+	nilSyscall := new(types.Syscall)
+	return !sameArgs(nilSyscall, config)
+}
+
+func identical(config1, config2 *types.Syscall) bool {
+	return reflect.DeepEqual(config1, config2)
+}
+
+func identicalExceptAction(config1, config2 *types.Syscall) bool {
+	samename := sameName(config1, config2)
+	sameAction := sameAction(config1, config2)
+	sameArgs := sameArgs(config1, config2)
+
+	return samename && !sameAction && sameArgs
+}
+
+func identicalExceptArgs(config1, config2 *types.Syscall) bool {
+	samename := sameName(config1, config2)
+	sameAction := sameAction(config1, config2)
+	sameArgs := sameArgs(config1, config2)
+
+	return samename && sameAction && !sameArgs
+}
+
+func sameName(config1, config2 *types.Syscall) bool {
+	return config1.Name == config2.Name
+}
+
+func sameAction(config1, config2 *types.Syscall) bool {
+	return config1.Action == config2.Action
+}
+
+func sameArgs(config1, config2 *types.Syscall) bool {
+	return reflect.DeepEqual(config1.Args, config2.Args)
+}
+
+func bothHaveArgs(config1, config2 *types.Syscall) bool {
+	return hasArguments(config1) && hasArguments(config2)
+}
+
+func onlyOneHasArgs(config1, config2 *types.Syscall) bool {
+	conf1 := hasArguments(config1)
+	conf2 := hasArguments(config2)
+
+	return (conf1 && !conf2) || (!conf1 && conf2)
+}
+
+func neitherHasArgs(config1, config2 *types.Syscall) bool {
+	return !hasArguments(config1) && !hasArguments(config2)
+}
+
+func firstParamOnlyHasArgs(config1, config2 *types.Syscall) bool {
+	return !hasArguments(config1) && hasArguments(config2)
+}